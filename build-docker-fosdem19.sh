b5bd871c0ce05cb72e5c853a60326c4e4839fec5
mehrad-rafigh <mehrad@rafigh.de>
2018-12-28 00:26:24 +0100

    ci(docker): add docker hub credentials

--- build-docker-fosdem19.sh
+++ build-docker-fosdem19.sh
@@ -2,7 +2,6 @@
 
 set -euo
 set -x
-
-declare -r GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-
-docker build -t mehrad-rafigh/fosdem19:${GIT_TAG} -f Dockerfile.fosdem19 .
+# declare -r GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+docker build -t mehradrafigh/fosdem-2019-schedule:latest -f Dockerfile.fosdem19 .
+docker push mehradrafigh/fosdem-2019-schedule:latest
